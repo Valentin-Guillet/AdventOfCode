fcb3a8598a97733471fa130a93501ae57a8de133
Val95240 <guillet.valentin95240@gmail.com>
2023-12-15 15:38:50 +0100

    Update test_all_days

--- Day0/test_all_days.sh
+++ Day0/test_all_days.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 # Execute this file in a year directory (e.g. in ../2017/) to test every day
 
+echo "Making all targets..."
 for i in {1..25}
 do
     cd ./Day$i 2>/dev/null || continue
-    echo "Day $i"
     [ -h Makefile ] || ln -s ../../Day0/Makefile Makefile
+    make --question && cd .. && continue
+    echo -n "Day $i "
     make
+    echo
+    cd ..
+done
+
+echo "Executing !"
+for i in {1..25}
+do
+    cd ./Day$i 2> /dev/null || continue
+    echo "Day $i"
     ./main
     ./main -s
     echo
