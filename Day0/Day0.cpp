e7ce11a3971170441a22b75c94b067d20fd57f2c
Val95240 <guillet.valentin95240@gmail.com>
2024-01-12 13:55:13 +0100

    Update Day0 to google style

--- Day0/Day0.cpp
+++ Day0/Day0.cpp
@@ -3,31 +3,29 @@
 #include <iostream>
 #include <string>
 
+int main(int argc, char *argv[]) {
+  bool first_half = true;
+  int ans = 0;
 
-int main(int argc, char* argv[])
-{
-    bool first_half = true;
-    int ans = 0;
+  std::ifstream myfile("input");
+  for (int i = 1; i < argc; ++i) {
+    std::string arg(argv[i]);
+    if (arg == "-t" || arg == "--test")
+      myfile = std::ifstream("test_input");
 
-    std::ifstream myfile("input");
-    for (int i = 1; i < argc; ++i) {
-        std::string arg(argv[i]);
-        if (arg == "-t" || arg == "--test")
-            myfile = std::ifstream("test_input");
+    else if (arg == "-s" || arg == "--second")
+      first_half = false;
+  }
 
-        else if (arg == "-s" || arg == "--second")
-            first_half = false;
-    }
+  if (myfile.is_open()) {
+    std::string input;
+    getline(myfile, input);
+    myfile.close();
+  } else {
+    std::cout << "Unable to open file" << std::endl;
+    return 1;
+  }
 
-    if (myfile.is_open()) {
-        std::string input;
-        getline(myfile, input);
-        myfile.close();
-    } else {
-        std::cout << "Unable to open file" << std::endl;
-        return 1;
-    }
-
-    std::cout << "Answer : " << ans << std::endl;
-    return 0;
+  std::cout << "Answer : " << ans << std::endl;
+  return 0;
 }
