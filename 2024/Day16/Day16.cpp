bfa0e7f2c2c1d309b0ae7c4a533d2e150595fc32
Valentin-Guillet <guillet.valentin95240@gmail.com>
2024-12-18 20:08:27 +0100

    Remove priority queue + debug print

--- 2024/Day16/Day16.cpp
+++ 2024/Day16/Day16.cpp
@@ -1,7 +1,6 @@
 
 #include <fstream>
 #include <iostream>
-#include <queue>
 #include <string>
 #include <vector>
 
@@ -32,6 +31,21 @@ struct Node {
 
 using Grid = std::vector<std::vector<std::vector<Node>>>;
 
+Pos get_shortest_dist(const Grid &grid, std::vector<Pos> &border) {
+  std::vector<Pos>::iterator shortest_dist_it;
+  int min_dist = 1000000;
+  for (auto it = border.begin(); it != border.end(); ++it) {
+    int dist = grid.at(it->row).at(it->col).at(it->dir).distance;
+    if (dist < min_dist) {
+      min_dist = dist;
+      shortest_dist_it = it;
+    }
+  }
+  Pos pos = *shortest_dist_it;
+  border.erase(shortest_dist_it);
+  return pos;
+}
+
 Pos get_next_pos(const Pos &pos) {
   if (pos.dir == NORTH)
     return Pos{pos.row - 1, pos.col, pos.dir};
@@ -58,7 +72,7 @@ int advance_pos(const Grid &grid, Pos &pos) {
     next_pos = get_next_pos(next_pos);
 
     const Node &node = grid.at(next_pos.row).at(next_pos.col).at(next_pos.dir);
-    if (node.value == '#' || node.seen)
+    if (node.value == '#')
       continue;
 
     if (next_found) // Found several exits: we're on a node
@@ -79,49 +93,57 @@ int advance_pos(const Grid &grid, Pos &pos) {
 int get_shortest_path(Grid &grid, int start_row, int start_col) {
   grid[start_row][start_col][EAST].distance = 0;
 
-  auto cmp = [&grid](Pos &left, Pos &right) {
-    return grid[left.row][left.col][left.dir].distance > grid[right.row][right.col][right.dir].distance;
-  };
-  std::priority_queue<Pos, std::vector<Pos>, decltype(cmp)> border(cmp);
-  border.push(Pos{start_row, start_col, EAST});
-
+  std::vector<Pos> border = { Pos{start_row, start_col, EAST} };
   while (!border.empty()) {
-    Pos curr_pos = border.top();
-    border.pop();
+    Pos curr_pos = get_shortest_dist(grid, border);
 
     Node &curr_node = grid[curr_pos.row][curr_pos.col][curr_pos.dir];
-
     if (curr_node.value == 'E')
       return curr_node.distance;
-
-    curr_node.seen = true;
 
     for (int ddir = 0; ddir < 4; ++ddir) {
       if (ddir == 2) // No half-turn
         continue;
 
       Pos pos{curr_pos.row, curr_pos.col, (curr_pos.dir + ddir) % 4};
-      if (ddir != 0 && grid[pos.row][pos.col][pos.dir].seen)
+      std::cout << "Pos = " << pos << "\n";
+      Node &node = grid[pos.row][pos.col][pos.dir];
+      if (node.seen) {
+        std::cout << "  => Node already seen !\n";
         continue;
+      }
 
-      pos = get_next_pos(pos);
-      if (grid[pos.row][pos.col][pos.dir].value == '#')
+
+      Pos next_pos = get_next_pos(pos);
+      if (grid[next_pos.row][next_pos.col][next_pos.dir].value == '#') {
+        std::cout << "  => Next pos " << next_pos << " is a wall!\n";
         continue;
+      }
+      std::cout << "  Marking pos " << pos << " as seen\n";
+      pos = next_pos;
+      node.seen = true;
 
       int dist_add = (ddir == 0 ? 0 : 1000);
       int step = 1;
       do {
+        std::cout << "    -> Advance to " << pos << " with added dist " << dist_add << " and step " << step << "\n";
         dist_add += step;
         step = advance_pos(grid, pos);
       } while (step > 0);
 
-      if (step == -1) // Dead end or node already seen
+      if (step == -1) { // Dead end
+        std::cout << "  -> Dead end!\n";
         continue;
+      }
 
       Node &next_node = grid[pos.row][pos.col][pos.dir];
+      if (next_node.seen && next_node.distance <= curr_node.distance + dist_add)
+        std::cout << "YOOOOOOOOOOOOOOOOOOOOOOO\n";
+
       if (next_node.distance == -1) {
         next_node.distance = curr_node.distance + dist_add;
-        border.push(pos);
+        std::cout << "  Adding " << pos << " with dist " << next_node.distance << " to the border\n";
+        border.push_back(pos);
       } else if (next_node.distance > curr_node.distance + dist_add) {
         next_node.distance = curr_node.distance + dist_add;
       }
